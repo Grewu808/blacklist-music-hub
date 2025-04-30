8024b7b7b2962c1016be4d01267600cc816fdd8a
Grewu808 <cristian.zdrang@yahoo.com>
2025-04-30 16:09:37 +0300

    Update graph.js

--- graph.js
+++ graph.js
@@ -123,7 +123,7 @@ function expandNode(event, clickedNode) {
   console.log("Se extinde nodul:", clickedNode.id);
 
   // !!!!! IMPORTANT !!!!! Înlocuiește 'YOUR_API_KEY' cu cheia ta API reală de la Last.fm
-  const apiKey = 'YOUR_API_KEY';
+  const apiKey = 'Y2d89aac23be0191cf4c48570759af0e9';
   // !!!!!!!!!!!!!!!!!!!!!
 
   const artistName = clickedNode.id; // Numele artistului pe care s-a dat click
